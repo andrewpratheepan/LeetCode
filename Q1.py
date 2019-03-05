cd167e391a24d63d7b5a9c43deed7988ad2db289
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-03-04 22:53:53 -0500

    first commit

--- Q1.py
+++
@@ -1,6 +0,0 @@
-def twoSum(nums, target:int):
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if (nums[i])+(nums[j]) == target and i!=j:
-                return [i,j]
-
