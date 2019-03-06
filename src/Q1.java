e8225eda9cb1d7347339850bbaa53ef8a98bf215
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-03-05 22:13:36 -0500

    added files and support package

--- src/Q1.java
+++ src/Q1.java
@@ -1,3 +1,14 @@
 public class Q1 {
-
+    public int[] twoSum(int[] nums, int target) {
+        int[] tar = new int[2];
+        for (int i = 0; i< nums.length; i++){
+            for (int j=i+1; j<nums.length;j++) {
+                if (nums[i]+nums[j]==target){
+                    tar[0]=i;tar[1]=j;
+                    break;
+                }
+            }
+        }
+        return tar;
+    }
 }
