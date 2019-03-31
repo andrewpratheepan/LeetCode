4afd222498e859375280146477c144b0b57f4236
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-03-31 19:58:02 -0400

    Q70

---
+++ src/Q70.java
@@ -0,0 +1,12 @@
+public class Q70 {
+    public int climbStairs(int n) {
+        if (n==1) return 1;
+        int n1 = 1, n2 = 2;
+        for (int i = 3; i<=n;i++){
+            int n3 = n1+n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n2;
+    }
+}
