ff43fa74a7af4f3fefe04816be7aeaefbd5932a9
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-04-04 21:18:32 -0400

    Q101

---
+++ src/Q101.java
@@ -0,0 +1,18 @@
+import supportingFiles.TreeNode;
+
+
+public class Q101 {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root,root);
+    }
+
+    public boolean isSymmetric(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null){
+            return true;
+        }
+        if (root1 == null || root2 == null){
+            return false;
+        }
+        return (root1.val == root2.val) && isSymmetric(root1.right, root2.left) && isSymmetric(root1.left,root2.right);
+    }
+}
