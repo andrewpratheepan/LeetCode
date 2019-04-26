5a59130d050530603926c15666d83437210f7fe2
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-04-26 17:12:00 +0100

    Q160

---
+++ src/Q160.java
@@ -0,0 +1,14 @@
+import supportingFiles.ListNode;
+
+
+public class Q160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode A = headA,B = headB;
+        while (A != B){
+            A = (A==null) ? headB:A.next;
+            B = (B==null) ? headA:B.next;
+        }
+        return A;
+    }
+}
