9a8d12630879ff9365ae252259a5cdcf4b7b93b7
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-05-29 07:45:44 -0400

    Q326

---
+++ src/Q326.java
@@ -0,0 +1,11 @@
+public class Q326 {
+    public boolean isPowerOfThree(int n) {
+        if (n%2 == 0){
+            return false;
+        }
+        while (n%3 == 0){
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
