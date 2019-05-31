b42802a699f37e59dd231a944195acf69bcfb5c5
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-05-31 08:17:37 -0400

    Q344

---
+++ src/Q344.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Q344 {
+    public void reverseString(char[] s) {
+        char temp;
+        for (int i = 0; i<s.length/2;i++) {
+            temp = s[i];
+            s[i] = s[s.length-i-1];
+            s[s.length-i-1] = temp;
+        }
+    }
+}
