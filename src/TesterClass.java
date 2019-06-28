162e75856ff3922a5263598f0ffa076d4dc555a2
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-06-28 06:30:52 -0400

    Q509

--- src/TesterClass.java
+++ src/TesterClass.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class TesterClass {
     public static void main(String args[]){
-    Q507 test = new Q507();
+    Q509 test = new Q509();
     int[] o = new int[]{4,1,2};
     int[] i = new int[]{10,3,8,9,4};
 
-    System.out.print(test.checkPerfectNumber(1));
+    System.out.print(test.fib(3));
 
     }
 }
