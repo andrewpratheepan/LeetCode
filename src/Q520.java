77ec5dfc0bbf4eca9f925e13e4349efd00e44f7e
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-06-29 23:27:28 -0400

    Q520 actual

--- src/Q520.java
+++ src/Q520.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Q520 {
+    public boolean detectCapitalUse(String word) {
+        boolean captials=true, lowercase=true, firstCapital=true;
+        for(int i=0; i<word.length(); i++){
+            if(word.charAt(i)>=97 && word.charAt(i)<=122){captials=false;break;}
+        }
+
+        for(int i=0; i<word.length(); i++){
+            if(word.charAt(i)>=65 && word.charAt(i)<=90){lowercase=false;break;}
+        }
+
+        if(word.charAt(0)>=97 && word.charAt(0)<=122)firstCapital=false;
+
+        if(firstCapital){
+            for(int i=1; i<word.length(); i++){
+                if(word.charAt(i)>=60 && word.charAt(i)<=95)firstCapital=false;
+            }
+        }
+
+        if(captials)return true;
+        if(lowercase)return true;
+        if(firstCapital)return true;
+        return false;
+    }
 }
